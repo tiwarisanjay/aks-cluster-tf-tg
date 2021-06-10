59faafaf2b168d9a7cee93ab799c2aeab50a6261
Sanjay Tiwari <65133206+sanjaytiwari007@users.noreply.github.com>
2021-06-10 15:31:44 -0400

    Update code for windows and linux node pool

--- 01-main.tf
+++ 01-main.tf
@@ -29,8 +29,8 @@ terraform {
 # Terraform State Storage to Azure Storage Container
   backend "azurerm" {
     resource_group_name   = "terraform-storage-rg"
-    storage_account_name  = "terraformstatexlrwdrzs"
-    container_name        = "tfstatefiles"
+    storage_account_name  = "nehastoragebackend"
+    container_name        = "tfstatfile"
     key                   = "dev.terraform.tfstate"
   }  
 }
